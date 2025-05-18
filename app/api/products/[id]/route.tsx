c3800f64231e7a90165e2fd399376281937c4221
Daniel <daniel-2193@hotmail.com>
2025-05-18 11:31:13 -0300

    product routes

--- app/api/products/[id]/route.tsx
+++ app/api/products/[id]/route.tsx
@@ -1,15 +1,56 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import { prisma } from "@/prisma/client";
+import { error } from "console";
 
-export function GET(
-    require: NextRequest,
-    { params }: { params: { name: string, price: number}}
-){
-    if(!params.name) {
-        return NextResponse.json({}) 
-    }
+export async function GET(request: NextRequest, { params }: { params: {id: string}}){
+    const product = await prisma.products.findUnique({
+        where: {id: parseInt(params.id)}
+    })
+
+    if(!product)
+        return NextResponse.json({error: 'Product not found'}, {status: 400})
+
+    return NextResponse.json(product)
 }
 
-export function PUT(){}
+export async function PUT(request: NextRequest, { params }: { params: {id: string}}){
+    const body = await request.json();
 
-export function DELETE(){}
+    const validation = schema.safeParse(body);
+
+    if(!validation.success)
+        return NextResponse.json(validation.error.errors, {status: 400});
+
+    const product = await prisma.products.findUnique({
+        where: { id: parseInt(params.id)}
+    })
+
+    if(!product)
+        return NextResponse.json({error: 'Product not found'}, {status: 400});
+
+    const newProduct = await prisma.products.update({
+        where: {id: product.id},
+        data:{
+            name: body.name,
+            price: body.price
+        }
+    })
+
+    return NextResponse.json(newProduct)
+}
+
+export async function DELETE(request: NextRequest, { params }: {params: {id: string}}){
+    const product = await prisma.products.findUnique({
+        where: { id: parseInt(params.id)}
+    })
+
+    if(!product)
+        return NextResponse.json({error:'Product not found!'}, {status: 400});
+
+    await prisma.products.delete({
+        where: { id: product.id}
+    });
+
+    return NextResponse.json({})
+}
--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,17 +1,31 @@
 import { NextResponse, NextRequest } from "next/server";
 import schema from "./schema";
+import { prisma } from "@/prisma/client";
+import { error } from "console";
 
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        {id: 1, name: 'Milk', price: 2.5},
-        {id: 2, name: 'Bread', price: 3.5}
-    ])
+export async function GET(request: NextRequest) {
+    const products = await prisma.products.findMany();
+    return NextResponse.json(products)
 }
 
 export async function POST(request: NextRequest){
     const body = await request.json();
-    const validation = schema.safeParse(body)
+    const validation = schema.safeParse(body);
     if(!validation.success)
         return NextResponse.json(validation.error.errors, {status: 400})
-    return NextResponse.json({id: 10, name: body.name, price: body.price}, {status: 201})
+    
+    // const product = await prisma.products.findUnique({
+    //     where: { id: body.id}
+    // })
+
+    // if(product)
+    //     return NextResponse.json({error:'Product already exists!'}, {status: 400});
+
+    const newProduct = await prisma.products.create({
+        data: {
+            name: body.name,
+            price: body.price
+        }
+    })
+    return NextResponse.json(newProduct, {status: 201})
 }
